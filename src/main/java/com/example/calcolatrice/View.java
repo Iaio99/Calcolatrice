db29df81ba27cf93e5e6f315bf81fcc8c670596a
Iaio99 <giuliano.vallone@ikmail.com>
2024-10-18 14:12:31 +0200

    Final style

--- src/main/java/com/example/calcolatrice/View.java
+++ src/main/java/com/example/calcolatrice/View.java
@@ -1,5 +1,6 @@
 package com.example.calcolatrice;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -8,71 +9,34 @@ public class View {
     private final Controller controller = new Controller();
     @FXML
     private TextField display;
-    private Button btn0;
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btn4;
-    private Button btn5;
-    private Button btn6;
-    private Button btn7;
-    private Button btn8;
-    private Button btn9;
-    private Button btnAdd;
-    private Button btnSubtract;
-    private Button btnMultiply;
-    private Button btnDivide;
-    private Button btnEquals;
-    private Button btnClear;
-    private Button btnPower;
-    private Button btnSqrt;
     private double firstOperand = 0;
     private String operator = "";
 
-    public void initialize() {
-        // Imposta gli eventi dei bottoni
-        btn0.setOnAction(e -> appendToDisplay("0"));
-        btn1.setOnAction(e -> appendToDisplay("1"));
-        btn2.setOnAction(e -> appendToDisplay("2"));
-        btn3.setOnAction(e -> appendToDisplay("3"));
-        btn4.setOnAction(e -> appendToDisplay("4"));
-        btn5.setOnAction(e -> appendToDisplay("5"));
-        btn6.setOnAction(e -> appendToDisplay("6"));
-        btn7.setOnAction(e -> appendToDisplay("7"));
-        btn8.setOnAction(e -> appendToDisplay("8"));
-        btn9.setOnAction(e -> appendToDisplay("9"));
-
-        btnAdd.setOnAction(e -> setOperator("+"));
-        btnSubtract.setOnAction(e -> setOperator("-"));
-        btnMultiply.setOnAction(e -> setOperator("*"));
-        btnDivide.setOnAction(e -> setOperator("/"));
-
-        btnPower.setOnAction(e -> setOperator("^"));
-
-        btnEquals.setOnAction(e -> calculate());
-        btnClear.setOnAction(e -> clearDisplay());
-    }
-
-    private void appendToDisplay(String value) {
-        display.setText(display.getText() + value);
-    }
-
-    private void setOperator(String op) {
-        firstOperand = Double.parseDouble(display.getText());
-        operator = op;
-        display.clear();
-    }
-
     @FXML
-    protected void calculate() {
-        double secondOperand = Double.parseDouble(display.getText());
-        Operation operation = controller.doOperation(firstOperand, secondOperand, operator);
-        double result = operation.getResult();
-        display.setText(String.valueOf(result));
+    private void handleButtonPress(ActionEvent event) {
+        String buttonText = ((Button) event.getSource()).getText();
+
+        if (buttonText.equals("C")) {
+            display.clear();
+            firstOperand = 0;
+            operator = "";
+        } else if (buttonText.equals("=")) {
+            double secondOperand = Double.parseDouble(display.getText());
+            Operation op = controller.doOperation(firstOperand, secondOperand, operator);
+            display.setText(String.valueOf(op.getResult()));
+            operator = ""; // Reset operator
+        } else {
+            if (isOperator(buttonText)) {
+                firstOperand = Double.parseDouble(display.getText());
+                operator = buttonText;
+                display.clear();
+            } else {
+                display.appendText(buttonText);
+            }
+        }
     }
 
-    private void clearDisplay() {
-        display.clear();
-        operator = "";
+    private boolean isOperator(String text) {
+        return text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/");
     }
 }
